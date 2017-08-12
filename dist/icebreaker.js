eca76220e68d1efcea4ec825190ea4bfeaecf652
alligator-io <markus@sonium.org>
2017-08-12 23:59:58 +0200

    Update dist

--- dist/icebreaker.js
+++ dist/icebreaker.js
@@ -1357,7 +1357,7 @@ var flatten = require('pull-stream/throughs/flatten');
 
 module.exports = function () {
   return pull(
-    values(arguments),
+    values([].slice.call(arguments)),
     map(function (arg) {
       if (!Array.isArray(arg)) return [arg]
       return arg
