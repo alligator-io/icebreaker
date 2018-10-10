ac2a363b66f33988d7e2d7e0001127c9c5a84f37
alligator-io <markus@sonium.org>
2018-10-10 20:37:06 +0200

    version++

--- dist/icebreaker.js
+++ dist/icebreaker.js
@@ -1,4 +1,4 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.icebreaker = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.icebreaker = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
  'use strict'
  module.exports = require('./pull');
 
@@ -777,8 +777,12 @@ module.exports = function asyncMap (map) {
       if(aborted) return cb(aborted)
       if(abort) {
         aborted = abort
-        if(!busy) read(abort, cb)
-        else read(abort, function () {
+        if(!busy) read(abort, function (err) {
+          //incase the source has already ended normally,
+          //we should pass our own error.
+          cb(abort)
+        })
+        else read(abort, function (err) {
           //if we are still busy, wait for the mapper to complete.
           if(busy) abortCb = cb
           else cb(abort)
@@ -794,7 +798,7 @@ module.exports = function asyncMap (map) {
               busy = false
               if(aborted) {
                 cb(aborted)
-                abortCb(aborted)
+                abortCb && abortCb(aborted)
               }
               else if(err) next (err, cb)
               else cb(null, data)
@@ -804,6 +808,11 @@ module.exports = function asyncMap (map) {
     }
   }
 }
+
+
+
+
+
 
 
 
@@ -970,7 +979,7 @@ module.exports = function take (test, opts) {
     }
 
     return function (end, cb) {
-      if(ended)            last ? terminate(cb) : cb(ended)
+      if(ended && !end) last ? terminate(cb) : cb(ended)
       else if(ended = end) read(ended, cb)
       else
         read(null, function (end, data) {
